dc620727a04bba17868ad8cce48c8c8134c22fe9
ridhaaf <ridhaaf@gmail.com>
2022-07-31 21:33:57 +0700

    Initial Commit

---
+++ WonderfulIndonesia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WonderfulIndonesia
+//
+//  Created by Ridha Ahmad Firdaus on 31/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WonderfulIndonesia/WonderfulIndonesiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WonderfulIndonesiaApp.swift
+//  WonderfulIndonesia
+//
+//  Created by Ridha Ahmad Firdaus on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WonderfulIndonesiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
